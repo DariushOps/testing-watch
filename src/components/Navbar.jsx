f43c1ad014b3a2e858bdfee43ff7a4582ab7184b
DariushOps <overlord127001@gmail.com>
2025-11-16 20:58:06 +0330

    adding and editing elements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,44 @@
 import { Link, NavLink } from "react-router-dom";
+import { PiShoppingBagOpenFill } from "react-icons/pi";
+
+function CartIcon({ count }) {
+  return (
+    <div className="relative">
+      <PiShoppingBagOpenFill className="w-8 h-8 text-shadow-white" />
+
+      {count > 0 && (
+        <span className="absolute -top-2 -right-2 bg-red-700 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
+          {count}
+        </span>
+      )}
+    </div>
+  );
+}
 
 export default function Navbar() {
+  const linkClasses = `hover:scale-120 transition-all duration-300 backface-hidden`;
   return (
-    <nav className="flex fixed top-0 left-0 right-0 font-montserrat font-medium text-stone-50 justify-between bg-slate-700 p-6 ">
-      <div className="flex items-center justify-center  gap-2 text-2xl">
-        <img src="vite.svg" alt="" />
+    <nav className="flex fixed top-0 left-0 right-0 font-montserrat font-medium text-stone-50  justify-between bg-linear-to-br from-emerald-800 to-cyan-800 shadow-lg p-6 ">
+      <div className="flex items-center justify-center  gap-2 text-2xl animate-fadeIn ">
+        <img src="logo.svg" alt="logo page" className="w-full h-8" />
         <h3>DariushOps</h3>
       </div>
-      <div className="flex items-center justify-center gap-4 p-[-2rem]">
-        <NavLink to="/">Home</NavLink>
-        <NavLink title="Shop" to="shop">
+      <div className="flex items-center justify-center gap-4  animate-fadeIn  ">
+        <NavLink className={linkClasses} to="/">
+          Home
+        </NavLink>
+        <NavLink className={linkClasses} to="shop">
           Shop
         </NavLink>
-        <NavLink title="About" to="about">
+        <NavLink className={linkClasses} to="about">
           About
         </NavLink>
-        <NavLink title="Contact Us" to="contactUs">
+        <NavLink className={linkClasses} to="contactUs">
           Contact Us
         </NavLink>
       </div>
-      <div className="flex items-center justify-center  gap-2">
+      <div className="flex items-center justify-center  gap-5 animate-fadeIn">
+        <CartIcon count={2} />
         <Link to="login">Log In</Link>
         <Link to="signup">Sign Up</Link>
       </div>
